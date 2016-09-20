ca5701d4837538118cc8869ea9e845a03ed17846
mulicvedad <mulic.vedad@gmail.com>
2016-09-21 00:36:26 +0200

    uploading code skeleton

---
+++ MovieApp/MovieApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieApp
+//
+//  Created by user on 21/09/16.
+//  Copyright © 2016 internshipABH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MovieApp/MovieApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MovieApp
+//
+//  Created by user on 21/09/16.
+//  Copyright © 2016 internshipABH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
